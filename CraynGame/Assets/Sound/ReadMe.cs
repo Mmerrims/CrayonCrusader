c1802125e2a5cf254fe27114bfefee897f191137
JarkTheShark <jdickerson@mail.bradley.edu>
2025-02-21 14:39:27 -0600

    The doors have audio now!

--- CraynGame/Assets/Sound/ReadMe.cs
+++ CraynGame/Assets/Sound/ReadMe.cs
@@ -1,4 +1,5 @@
 //15GGalasovaK_crayon.wav by KaterinaGalasova -- https://freesound.org/s/461928/ -- License: Creative Commons 0
 //Thump by Baconation -- https://freesound.org/s/592455/ -- License: Creative Commons 0
 //Knife / sword swing by spycrah -- https://freesound.org/s/471097/ -- License: Creative Commons 0
+//Lever Pull - lever.wav by Krokulator -- https://freesound.org/s/696746/ -- License: Creative Commons 0
 
