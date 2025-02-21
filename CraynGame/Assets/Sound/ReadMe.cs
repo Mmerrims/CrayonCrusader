7f4dc2da25e8a9e0947d9ba82f846b0161c14b26
JarkTheShark <jdickerson@mail.bradley.edu>
2025-02-21 14:14:41 -0600

    Added Swing and Hit sounds
    Swings are technically implemented but are ruled out atm due to them going 3-4 times per press

--- CraynGame/Assets/Sound/ReadMe.cs
+++ CraynGame/Assets/Sound/ReadMe.cs
@@ -1,2 +1,4 @@
 //15GGalasovaK_crayon.wav by KaterinaGalasova -- https://freesound.org/s/461928/ -- License: Creative Commons 0
+//Thump by Baconation -- https://freesound.org/s/592455/ -- License: Creative Commons 0
+//Knife / sword swing by spycrah -- https://freesound.org/s/471097/ -- License: Creative Commons 0
 
